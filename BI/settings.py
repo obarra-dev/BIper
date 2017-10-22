def350e09f391f4f1a950b00e1f7bd93ed4b97ff
omar <barraomar12@gmail.com>
2017-10-22 17:08:56 -0300

    'bd'

--- BI/settings.py
+++ BI/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'BI.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -99,7 +99,7 @@ DATABASES = {
 	)
 }
 
-"""
+
 
 
 # Password validation
